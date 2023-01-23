a29d599261d4e07a8851ee2dd1a4e5f42bfce77a
Shlomi Israely <sijproject@gmail.com>
2023-01-23 20:06:29 +0100

    html plugin

--- webpack/devServer.js
+++ webpack/devServer.js
@@ -1,47 +1,34 @@
 import Webpack from "webpack";
 import WebpackDevServer from "webpack-dev-server";
 
-import express from "express";
-
 import webpackConfig from "../webpack.config";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import React from "react";
+import ReactDOMServer from "react-dom/server";
 
-import fs from "fs";
-import path from "path";
+import App from "../src/App";
+
+// import fs from "fs";
+// import path from "path";
 import { renderApp } from "../src/dev/renderApp";
 
-const compiler = Webpack(webpackConfig);
+const compiler = Webpack({
+  ...webpackConfig,
+  plugins: [
+    new HtmlWebpackPlugin({
+      templateParameters: {
+        content: renderApp()
+      },
+      filename: "index.html",
+      template: "public/index.html"
+    })
+  ]
+});
 
 const devServerOptions = {
   ...webpackConfig.devServer,
-  setupMiddlewares(middleware, devServer) {
-    if (!devServer) {
-      throw new Error("webpack-dev-server is not defined");
-    }
-
-    middleware.push({
-      name: "ssr",
-      path: "/",
-      middleware(req, res) {
-        fs.readFile(
-          path.join(__dirname, "../public/index.html"),
-          (err, data) => {
-            if (err) res.send("error");
-
-            res.send(
-              data
-                .toString()
-                .replace(
-                  `<div id="root"></div>`,
-                  `<div id="root">${renderApp()}</div>`
-                )
-            );
-          }
-        );
-      }
-    });
-    middleware.push(express.static(path.join(__dirname, "../public")));
-
-    return middleware;
+  proxy: {
+    "*": "http://localhost:3000/dist/index.html"
   }
 };
 
